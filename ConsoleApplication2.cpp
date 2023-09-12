10efdbc0e6d0cc225a9006f855e3b2b0ab569fe3
fisenko11 <144794898+fisenko11@users.noreply.github.com>
2023-09-12 14:43:33 +0300

    СppFuncProjectHW
    Выполнение Самостоятельных Работ C++

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a, b;
+	cout << "input a:";
+	cin >> a;
+	cout << "input b:";
+	cin >> b;
+	cout << a << "+" << b << "=" << a + b << endl;
+	cout << a << "-" << b << "=" << a - b << endl;
+	cout << a << "*" << b << "=" << a * b << endl;
+	cout << a << "/" << b << "=" << a / b << endl;
+	
+
+
+	
+
+
+}
+
